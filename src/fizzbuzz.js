9e774e1af6b4414beb468c174dc02f926237b2ab
Xplay518 <gustavo.valencia@ucb.edu.bo.com>
2024-02-25 23:48:59 -0400

    Ya se tiene el 2do punto

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,9 +1,15 @@
 function generarFizzBuzz(numero) {
+    let resultado = "";
+
     if (numero % 3 === 0) {
-        return "Fizz";
-    } else {
-        return numero.toString();
+        resultado += "Fizz";
     }
+
+    if (numero % 5 === 0) {
+        resultado += "Buzz";
+    }
+
+    return resultado || numero.toString();
 }
 
 export default generarFizzBuzz;
--- src/fizzbuzz.spec.js
+++ src/fizzbuzz.spec.js
@@ -7,6 +7,18 @@ describe("FizzBuzz", () => {
     expect(generarFizzBuzz(9)).toEqual("Fizz");
   });
 
+  it("Debería imprimir 'Buzz' para números divisibles por 5", () => {
+    expect(generarFizzBuzz(5)).toEqual("Buzz");
+    expect(generarFizzBuzz(10)).toEqual("Buzz");
+    expect(generarFizzBuzz(20)).toEqual("Buzz");
+  });
+
+  it("Debería imprimir 'FizzBuzz' para números divisibles por 3 y 5", () => {
+    expect(generarFizzBuzz(15)).toEqual("FizzBuzz");
+    expect(generarFizzBuzz(30)).toEqual("FizzBuzz");
+    expect(generarFizzBuzz(45)).toEqual("FizzBuzz");
+  });
+
   it("Debería imprimir el número como una cadena para otros números", () => {
     expect(generarFizzBuzz(1)).toEqual("1");
     expect(generarFizzBuzz(2)).toEqual("2");
