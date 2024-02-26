ed2793e9ba01dd05f4f15c42d75f059e70e95d9f
Xplay518 <gustavo.valencia@ucb.edu.bo.com>
2024-02-25 23:45:14 -0400

    Ya se tiene el 1er punto

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,5 +1,9 @@
 function generarFizzBuzz(numero) {
-    return numero.toString();
+    if (numero % 3 === 0) {
+        return "Fizz";
+    } else {
+        return numero.toString();
+    }
 }
 
 export default generarFizzBuzz;
--- src/fizzbuzz.spec.js
+++ src/fizzbuzz.spec.js
@@ -1,7 +1,15 @@
 import generarFizzBuzz from "./fizzbuzz.js";
 
 describe("FizzBuzz", () => {
-  it("Deberia generar el mismo numero oara uno que no tiene reglas como el nro 1", () => {
+  it("Debería imprimir 'Fizz' para números divisibles por 3", () => {
+    expect(generarFizzBuzz(3)).toEqual("Fizz");
+    expect(generarFizzBuzz(6)).toEqual("Fizz");
+    expect(generarFizzBuzz(9)).toEqual("Fizz");
+  });
+
+  it("Debería imprimir el número como una cadena para otros números", () => {
     expect(generarFizzBuzz(1)).toEqual("1");
+    expect(generarFizzBuzz(2)).toEqual("2");
+    expect(generarFizzBuzz(4)).toEqual("4");
   });
 });
